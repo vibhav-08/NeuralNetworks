acfa9f41513a5397f5c790c1a5780b3acfaead4d
vibhav-08 <cs20b047@iittp.ac.in>
2023-02-22 15:51:51 +0530

    minor change

--- hubconf.py
+++ hubconf.py
@@ -81,8 +81,12 @@ def _train(dataloader, model, loss_fn, optimizer):
     model.train()
     for batch, (X, y) in enumerate(dataloader):
         X, y = X.to(device), y.to(device)
+
+        # Compute prediction error
         pred = model(X)
         loss = loss_fn(pred, y)
+
+        # Backpropagation
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
@@ -105,7 +109,7 @@ def _test(dataloader, model, loss_fn):
     test_loss /= num_batches
     correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
-
+    
 def train(train_dataloader, test_dataloader, model1, loss_fn1, optimizer1, epochs = 5):
     for t in range(epochs):
         print(f"Epoch {t+1}\n-------------------------------")
